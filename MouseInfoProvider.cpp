319e0a7236094856d27e1f3793d04e6a127e46d2
John Van Enk <vanenkj@gmail.com>
2013-12-19 09:24:47 -0500

    Tabs not as good as spaces.

--- MouseInfoProvider.cpp
+++ MouseInfoProvider.cpp
@@ -6,91 +6,91 @@
 
 namespace MouseInfo
 {
-	struct icon_info
-	{
-		LPWSTR id;
-		enum CursorIconType type;
-		HCURSOR cur;
-	};
+    struct icon_info
+    {
+        LPWSTR id;
+        enum CursorIconType type;
+        HCURSOR cur;
+    };
 
-	static struct icon_info icons[] =
-	{
-	  { IDC_APPSTARTING, IDC_APPSTARTING_TYPE, NULL },
-	  { IDC_ARROW, IDC_ARROW_TYPE, NULL },
-	  { IDC_CROSS, IDC_CROSS_TYPE, NULL },
-	  { IDC_HAND, IDC_HAND_TYPE, NULL },
-	  { IDC_HELP, IDC_HELP_TYPE, NULL },
-	  { IDC_IBEAM, IDC_IBEAM_TYPE, NULL },
-	  { IDC_NO, IDC_NO_TYPE, NULL },
-	  { IDC_SIZEALL, IDC_SIZEALL_TYPE, NULL },
-	  { IDC_SIZENESW, IDC_SIZENESW_TYPE, NULL },
-	  { IDC_SIZENS, IDC_SIZENS_TYPE, NULL },
-	  { IDC_SIZENWSE, IDC_SIZENWSE_TYPE, NULL },
-	  { IDC_SIZEWE, IDC_SIZEWE_TYPE, NULL },
-	  { IDC_UPARROW, IDC_UPARROW_TYPE, NULL },
-	  { IDC_WAIT, IDC_WAIT_TYPE, NULL },
-	  { IDI_APPLICATION, IDI_APPLICATION_TYPE, NULL },
-	  { IDI_ASTERISK, IDI_ASTERISK_TYPE, NULL },
-	  { IDI_EXCLAMATION, IDI_EXCLAMATION_TYPE, NULL },
-	  { IDI_HAND, IDI_HAND_TYPE, NULL },
-	  { IDI_QUESTION, IDI_QUESTION_TYPE, NULL },
-	  { IDI_WINLOGO, IDI_WINLOGO_TYPE, NULL },
-	};
+    static struct icon_info icons[] =
+    {
+      { IDC_APPSTARTING, IDC_APPSTARTING_TYPE, NULL },
+      { IDC_ARROW, IDC_ARROW_TYPE, NULL },
+      { IDC_CROSS, IDC_CROSS_TYPE, NULL },
+      { IDC_HAND, IDC_HAND_TYPE, NULL },
+      { IDC_HELP, IDC_HELP_TYPE, NULL },
+      { IDC_IBEAM, IDC_IBEAM_TYPE, NULL },
+      { IDC_NO, IDC_NO_TYPE, NULL },
+      { IDC_SIZEALL, IDC_SIZEALL_TYPE, NULL },
+      { IDC_SIZENESW, IDC_SIZENESW_TYPE, NULL },
+      { IDC_SIZENS, IDC_SIZENS_TYPE, NULL },
+      { IDC_SIZENWSE, IDC_SIZENWSE_TYPE, NULL },
+      { IDC_SIZEWE, IDC_SIZEWE_TYPE, NULL },
+      { IDC_UPARROW, IDC_UPARROW_TYPE, NULL },
+      { IDC_WAIT, IDC_WAIT_TYPE, NULL },
+      { IDI_APPLICATION, IDI_APPLICATION_TYPE, NULL },
+      { IDI_ASTERISK, IDI_ASTERISK_TYPE, NULL },
+      { IDI_EXCLAMATION, IDI_EXCLAMATION_TYPE, NULL },
+      { IDI_HAND, IDI_HAND_TYPE, NULL },
+      { IDI_QUESTION, IDI_QUESTION_TYPE, NULL },
+      { IDI_WINLOGO, IDI_WINLOGO_TYPE, NULL },
+    };
 
-	MouseInfoProvider::MouseInfoProvider(void)
-	{
-		info = (PCURSORINFO)malloc(sizeof(CURSORINFO));
+    MouseInfoProvider::MouseInfoProvider(void)
+    {
+        info = (PCURSORINFO)malloc(sizeof(CURSORINFO));
 
-		for (int i = 0; i < sizeof(icons)/sizeof(icons[0]); i++) 
-		{
-			icons[i].cur = LoadCursor(NULL,icons[i].id);
-		}
-	}
+        for (int i = 0; i < sizeof(icons)/sizeof(icons[0]); i++) 
+        {
+            icons[i].cur = LoadCursor(NULL,icons[i].id);
+        }
+    }
 
-	enum CursorIconType MouseInfoProvider::GetCursorIconType(void)
-	{
-		HCURSOR hcur;
-		ICONINFO iconInfo;
+    enum CursorIconType MouseInfoProvider::GetCursorIconType(void)
+    {
+        HCURSOR hcur;
+        ICONINFO iconInfo;
 
-		info->cbSize = sizeof(CURSORINFO);
+        info->cbSize = sizeof(CURSORINFO);
 
-		if (!GetCursorInfo(info))
-		{
-			return CURSOR_UNKNOWN_TYPE;
-		}
+        if (!GetCursorInfo(info))
+        {
+            return CURSOR_UNKNOWN_TYPE;
+        }
 
-		hcur = info->hCursor;
+        hcur = info->hCursor;
 
-		if (!GetIconInfo(hcur, &iconInfo))
-		{
-			return CURSOR_UNKNOWN_TYPE;
-		}
+        if (!GetIconInfo(hcur, &iconInfo))
+        {
+            return CURSOR_UNKNOWN_TYPE;
+        }
 
-		for (int i = 0; i < sizeof(icons) / sizeof(icons[0]); i++)
-		{
-			if (hcur == icons[i].cur)
-			{
-				return icons[i].type;
-			}
-		}
+        for (int i = 0; i < sizeof(icons) / sizeof(icons[0]); i++)
+        {
+            if (hcur == icons[i].cur)
+            {
+                return icons[i].type;
+            }
+        }
 
-		return CURSOR_UNKNOWN_TYPE;
-	}
+        return CURSOR_UNKNOWN_TYPE;
+    }
 
-	void MouseInfoProvider::GetCursorPosition(POINT * point)
-	{
-		info->cbSize = sizeof(CURSORINFO);
+    void MouseInfoProvider::GetCursorPosition(POINT * point)
+    {
+        info->cbSize = sizeof(CURSORINFO);
 
-		if (!GetCursorInfo(info))
-		{
-			return; // TODO: This needs to be handled better.
-		}
+        if (!GetCursorInfo(info))
+        {
+            return; // TODO: This needs to be handled better.
+        }
 
-		memcpy(point, &info->ptScreenPos, sizeof(*point));
-	}
+        memcpy(point, &info->ptScreenPos, sizeof(*point));
+    }
 
-	MouseInfoProvider::~MouseInfoProvider(void)
-	{
-		free(info);
-	}
+    MouseInfoProvider::~MouseInfoProvider(void)
+    {
+        free(info);
+    }
 }
--- MouseInfoProvider.h
+++ MouseInfoProvider.h
@@ -4,40 +4,40 @@
 
 namespace MouseInfo
 {
-	public enum CursorIconType
-	{
-		CURSOR_UNKNOWN_TYPE = 0,
-		IDC_APPSTARTING_TYPE = 1,
-		IDC_ARROW_TYPE = 2,
-		IDC_CROSS_TYPE = 3,
-		IDC_HAND_TYPE = 4,
-		IDC_HELP_TYPE = 5,
-		IDC_IBEAM_TYPE = 6,
-		IDC_NO_TYPE = 7,
-		IDC_SIZEALL_TYPE = 8,
-		IDC_SIZENESW_TYPE = 9,
-		IDC_SIZENS_TYPE = 10,
-		IDC_SIZENWSE_TYPE = 11,
-		IDC_SIZEWE_TYPE = 12,
-		IDC_UPARROW_TYPE = 13,
-		IDC_WAIT_TYPE = 14,
-		IDI_APPLICATION_TYPE = 15,
-		IDI_ASTERISK_TYPE = 16,
-		IDI_EXCLAMATION_TYPE = 17,
-		IDI_HAND_TYPE = 18,
-		IDI_QUESTION_TYPE = 19,
-		IDI_WINLOGO_TYPE = 20,
-	};
+    public enum CursorIconType
+    {
+        CURSOR_UNKNOWN_TYPE = 0,
+        IDC_APPSTARTING_TYPE = 1,
+        IDC_ARROW_TYPE = 2,
+        IDC_CROSS_TYPE = 3,
+        IDC_HAND_TYPE = 4,
+        IDC_HELP_TYPE = 5,
+        IDC_IBEAM_TYPE = 6,
+        IDC_NO_TYPE = 7,
+        IDC_SIZEALL_TYPE = 8,
+        IDC_SIZENESW_TYPE = 9,
+        IDC_SIZENS_TYPE = 10,
+        IDC_SIZENWSE_TYPE = 11,
+        IDC_SIZEWE_TYPE = 12,
+        IDC_UPARROW_TYPE = 13,
+        IDC_WAIT_TYPE = 14,
+        IDI_APPLICATION_TYPE = 15,
+        IDI_ASTERISK_TYPE = 16,
+        IDI_EXCLAMATION_TYPE = 17,
+        IDI_HAND_TYPE = 18,
+        IDI_QUESTION_TYPE = 19,
+        IDI_WINLOGO_TYPE = 20,
+    };
 
-	public ref class MouseInfoProvider
-	{
-	public:
-		MouseInfoProvider(void);
-		~MouseInfoProvider(void);
+    public ref class MouseInfoProvider
+    {
+    public:
+        MouseInfoProvider(void);
+        ~MouseInfoProvider(void);
 
-		enum CursorIconType GetCursorIconType(void);
-		void GetCursorPosition(POINT * point);
-	private:
-		PCURSORINFO info;
-	};
+        enum CursorIconType GetCursorIconType(void);
+        void GetCursorPosition(POINT * point);
+    private:
+        PCURSORINFO info;
+    };
 }
